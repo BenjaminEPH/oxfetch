0e9514bbfb503c287e1a303ee8bdbd865fee5a06
ben <benjamin.ely07@gmail.com>
2026-04-04 18:54:03 -0600

    Finishing CLI mod

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,20 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[command(
+    name = "oxfetch",
+    version,
+    about = "A system info tool written in Rust."
+)]
+pub struct Cli {
+    #[arg(long, value_name = "DISTRO")]
+    pub logo: Option<String>,
 
+    #[arg(long, value_name = "FIELD")]
+    pub hide: Option<String>,
+
+    #[arg(long, value_name = "PATH")]
+    pub config: Option<String>,
+
+    #[arg(long, value_name = "THEME")]
+    pub theme: Option<String>,
+}
